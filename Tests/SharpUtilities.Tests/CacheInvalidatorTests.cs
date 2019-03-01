f1cdef1662ed5bbe629459a893e8dc7c5bccd87c
southpolenator <southpolenator@gmail.com>
2019-03-02 00:29:53 +0100

    Adding tests for CacheInvalidator.

--- Tests/SharpUtilities.Tests/CacheInvalidatorTests.cs
+++ Tests/SharpUtilities.Tests/CacheInvalidatorTests.cs
@@ -19,5 +19,40 @@ namespace SharpUtilities.Tests
             Assert.Equal(0, dictionaryCache.Count);
             Assert.False(simpleCache.Cached);
         }
+
+        [Fact]
+        public void Enumerate()
+        {
+            DictionaryCache<int, string> dictionaryCache;
+            SimpleCache<int> simpleCache;
+
+            using (CacheInvalidator cacheInvalidator = new CacheInvalidator())
+            {
+                dictionaryCache = cacheInvalidator.CreateDictionaryCache<int, string>(key => key.ToString());
+                simpleCache = cacheInvalidator.CreateSimpleCache(() => 42);
+                dictionaryCache.TryGetValue(simpleCache.Value, out string testValue);
+                Assert.NotEmpty(cacheInvalidator);
+            }
+        }
+
+        class CacheTestClass
+        {
+            public CacheInvalidator CacheInvalidator;
+        }
+
+        [Fact]
+        public void InvalidateCaches()
+        {
+            DictionaryCache<int, string> dictionaryCache;
+            SimpleCache<int> simpleCache;
+            CacheTestClass test = new CacheTestClass();
+            test.CacheInvalidator = new CacheInvalidator();
+            dictionaryCache = test.CacheInvalidator.CreateDictionaryCache<int, string>(key => key.ToString());
+            simpleCache = test.CacheInvalidator.CreateSimpleCache(() => 42);
+            dictionaryCache.TryGetValue(simpleCache.Value, out string testValue);
+            CacheInvalidator.InvalidateCaches(test);
+            Assert.Equal(0, dictionaryCache.Count);
+            Assert.False(simpleCache.Cached);
+        }
     }
 }
