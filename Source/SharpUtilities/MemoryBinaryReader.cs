abe978be9e5950ee2167d6954c1017242dadafba
southpolenator <southpolenator@gmail.com>
2019-03-02 16:03:45 +0100

    Adding MemoryBinaryReader

---
+++ Source/SharpUtilities/MemoryBinaryReader.cs
@@ -0,0 +1,198 @@
+﻿using System.Text;
+
+namespace SharpUtilities
+{
+    /// <summary>
+    /// Class that implements <see cref="IBinaryReader"/> for memory (pointer and length).
+    /// </summary>
+    public unsafe class MemoryBinaryReader : IBinaryReader
+    {
+        /// <summary>
+        /// Pointer to the start of memory buffer.
+        /// </summary>
+        private readonly byte* basePointer;
+
+        /// <summary>
+        /// Current position in the memory buffer.
+        /// </summary>
+        private byte* pointer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemoryBinaryReader"/> class.
+        /// </summary>
+        public MemoryBinaryReader(byte* memory, long length)
+        {
+            pointer = basePointer = memory;
+            Length = length;
+        }
+
+        /// <summary>
+        /// Gets the length of the stream in bytes.
+        /// </summary>
+        public long Length { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the position in the stream.
+        /// </summary>
+        public long Position
+        {
+            get => pointer - basePointer;
+            set => pointer = basePointer + value;
+        }
+
+        /// <summary>
+        /// Gets the remaining number of bytes in the stream.
+        /// </summary>
+        public long BytesRemaining => Length - Position;
+
+        /// <summary>
+        /// Creates duplicate of this stream.
+        /// </summary>
+        public virtual IBinaryReader Duplicate()
+        {
+            return new MemoryBinaryReader(basePointer, Length)
+            {
+                Position = Position,
+            };
+        }
+
+        /// <summary>
+        /// Reads <c>byte</c> from the stream.
+        /// </summary>
+        public byte ReadByte()
+        {
+            byte value = *pointer;
+
+            pointer++;
+            return value;
+        }
+
+        /// <summary>
+        /// Reads <c>short</c> from the stream.
+        /// </summary>
+        public short ReadShort()
+        {
+            short value = *((short*)pointer);
+
+            pointer += 2;
+            return value;
+        }
+
+        /// <summary>
+        /// Reads <c>ushort</c> from the stream.
+        /// </summary>
+        public ushort ReadUshort()
+        {
+            ushort value = *((ushort*)pointer);
+
+            pointer += 2;
+            return value;
+        }
+
+        /// <summary>
+        /// Reads <c>int</c> from the stream.
+        /// </summary>
+        public int ReadInt()
+        {
+            int value = *((int*)pointer);
+
+            pointer += 4;
+            return value;
+        }
+
+        /// <summary>
+        /// Reads <c>uint</c> from the stream.
+        /// </summary>
+        public uint ReadUint()
+        {
+            uint value = *((uint*)pointer);
+
+            pointer += 4;
+            return value;
+        }
+
+        /// <summary>
+        /// Reads <c>long</c> from the stream.
+        /// </summary>
+        public long ReadLong()
+        {
+            long value = *((long*)pointer);
+
+            pointer += 8;
+            return value;
+        }
+
+        /// <summary>
+        /// Reads <c>ulong</c> from the stream.
+        /// </summary>
+        public ulong ReadUlong()
+        {
+            ulong value = *((ulong*)pointer);
+
+            pointer += 8;
+            return value;
+        }
+
+        /// <summary>
+        /// Reads C-style string (null terminated) from the stream.
+        /// </summary>
+        public string ReadCString()
+        {
+            byte* start = pointer;
+            byte* end = start;
+
+            while (*end != 0)
+                end++;
+#if NET45
+            byte[] bytes = this.ReadByteArray((int)(end - start));
+
+            pointer++;
+            return Encoding.UTF8.GetString(bytes);
+#else
+            pointer = end + 1;
+            return Encoding.UTF8.GetString(start, (int)(end - start));
+#endif
+        }
+
+        /// <summary>
+        /// Reads C-style wide (2 bytes) string (null terminated) from the stream.
+        /// </summary>
+        public string ReadCStringWide()
+        {
+            ushort* start = (ushort*)pointer;
+            ushort* end = start;
+
+            while (*end != 0)
+                end++;
+#if NET45
+            byte[] bytes = this.ReadByteArray((int)((byte*)end - (byte*)start));
+
+            pointer += 2;
+            return Encoding.Unicode.GetString(bytes);
+#else
+            pointer = (byte*)(end + 1);
+            return Encoding.Unicode.GetString((byte*)start, (int)((byte*)end - (byte*)start));
+#endif
+        }
+
+        /// <summary>
+        /// Reads bytes buffer from the stream.
+        /// </summary>
+        /// <param name="bytes">Buffer pointer where bytes should be stored.</param>
+        /// <param name="count">Number of bytes to from the stream</param>
+        public void ReadBytes(byte* bytes, uint count)
+        {
+            MemoryBuffer.MemCpy(bytes, pointer, count);
+            pointer += count;
+        }
+
+        /// <summary>
+        /// Moves position by the specified bytes.
+        /// </summary>
+        /// <param name="bytes">Number of bytes to move the stream.</param>
+        public void Move(uint bytes)
+        {
+            pointer += bytes;
+        }
+    }
+}
---
+++ Tests/SharpUtilities.Tests/MemoryBinaryReaderTests.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using Xunit;
+
+namespace SharpUtilities.Tests
+{
+    public unsafe class MemoryBinaryReaderTests
+    {
+        [Fact]
+        public void ReadByte()
+        {
+            byte[] bytes = new byte[] { 127, 128, 129, 200 };
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+
+                for (int i = 0; i < bytes.Length; i++)
+                    Assert.Equal(bytes[i], reader.ReadByte());
+                Assert.Equal(0, reader.BytesRemaining);
+            }
+        }
+
+        [Fact]
+        public void ReadShort()
+        {
+            byte[] bytes = new byte[] { 127, 0, 255, 255, 0, 1 };
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+
+                Assert.Equal(127, reader.ReadShort());
+                Assert.Equal(-1, reader.ReadShort());
+                Assert.Equal(256, reader.ReadShort());
+                Assert.Equal(0, reader.BytesRemaining);
+            }
+        }
+
+        [Fact]
+        public void ReadUshort()
+        {
+            byte[] bytes = new byte[] { 127, 0, 255, 255, 0, 1 };
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+
+                Assert.Equal(127, reader.ReadUshort());
+                Assert.Equal(ushort.MaxValue, reader.ReadUshort());
+                Assert.Equal(256, reader.ReadUshort());
+                Assert.Equal(0, reader.BytesRemaining);
+            }
+        }
+
+        [Fact]
+        public void ReadInt()
+        {
+            byte[] bytes = new byte[] { 127, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 1 };
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+
+                Assert.Equal(127, reader.ReadInt());
+                Assert.Equal(-1, reader.ReadInt());
+                Assert.Equal(256 * 256 * 256, reader.ReadInt());
+                Assert.Equal(0, reader.BytesRemaining);
+            }
+        }
+
+        [Fact]
+        public void ReadUint()
+        {
+            byte[] bytes = new byte[] { 127, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 1 };
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+
+                Assert.Equal(127U, reader.ReadUint());
+                Assert.Equal(uint.MaxValue, reader.ReadUint());
+                Assert.Equal(256 * 256 * 256U, reader.ReadUint());
+                Assert.Equal(0, reader.BytesRemaining);
+            }
+        }
+
+        [Fact]
+        public void ReadLong()
+        {
+            byte[] bytes = new byte[] { 127, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0, 1, 0, 0, 0, 0 };
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+
+                Assert.Equal(127, reader.ReadLong());
+                Assert.Equal(-1, reader.ReadLong());
+                Assert.Equal(256 * 256 * 256, reader.ReadLong());
+                Assert.Equal(0, reader.BytesRemaining);
+            }
+        }
+
+        [Fact]
+        public void ReadUlong()
+        {
+            byte[] bytes = new byte[] { 127, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0, 1, 0, 0, 0, 0 };
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+
+                Assert.Equal(127U, reader.ReadUlong());
+                Assert.Equal(ulong.MaxValue, reader.ReadUlong());
+                Assert.Equal(256 * 256 * 256U, reader.ReadUlong());
+                Assert.Equal(0, reader.BytesRemaining);
+            }
+        }
+
+        [Fact]
+        public void ReadCString()
+        {
+            byte[] bytes = new byte[] { (byte)'T', (byte)'e', (byte)'s', (byte)'t', 0 };
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+
+                Assert.Equal("Test", reader.ReadCString());
+                Assert.Equal(0, reader.BytesRemaining);
+            }
+        }
+
+        [Fact]
+        public void ReadCStringWide()
+        {
+            byte[] bytes = new byte[] { (byte)'T', 0, (byte)'e', 0, (byte)'s', 0, (byte)'t', 0, 0, 0 };
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+
+                Assert.Equal("Test", reader.ReadCStringWide());
+                Assert.Equal(0, reader.BytesRemaining);
+            }
+        }
+
+        [Fact]
+        public unsafe void ReadBytes()
+        {
+            byte[] bytes = Guid.NewGuid().ToByteArray();
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+
+                byte[] readBytes = new byte[reader.BytesRemaining];
+
+                fixed (byte* b = readBytes)
+                {
+                    reader.ReadBytes(b, (uint)readBytes.Length);
+                }
+                Assert.Equal(bytes, readBytes);
+                Assert.Equal(0, reader.BytesRemaining);
+            }
+        }
+
+        [Fact]
+        public unsafe void Move()
+        {
+            byte[] bytes = Guid.NewGuid().ToByteArray();
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+
+                Assert.Equal(bytes.Length, reader.Length);
+                reader.Move((uint)bytes.Length / 2);
+                Assert.Equal(bytes.Length / 2, reader.BytesRemaining);
+                reader.Position += bytes.Length / 2;
+                Assert.Equal(0, reader.BytesRemaining);
+            }
+        }
+
+        [Fact]
+        public unsafe void Duplicate()
+        {
+            byte[] bytes = Guid.NewGuid().ToByteArray();
+
+            fixed (byte* memory = bytes)
+            {
+                MemoryBinaryReader reader = new MemoryBinaryReader(memory, bytes.Length);
+                reader.Position = 5;
+                IBinaryReader reader2 = reader.Duplicate();
+
+                Assert.Equal(reader.Position, reader2.Position);
+                reader.Position++;
+                Assert.NotEqual(reader.Position, reader2.Position);
+                reader2.Position++;
+                Assert.Equal(reader.Position, reader2.Position);
+                reader2.Position++;
+                Assert.NotEqual(reader.Position, reader2.Position);
+            }
+        }
+    }
+}
