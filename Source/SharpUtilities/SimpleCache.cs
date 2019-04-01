da227b3798e2e84e1bf0f1dd74c81afb548f3209
southpolenator <southpolenator@gmail.com>
2019-04-02 00:49:41 +0200

    Adding SimpleCacheWithContext to reduce memory footprint and allocation count with smaller objects.

--- Source/SharpUtilities/SimpleCache.cs
+++ Source/SharpUtilities/SimpleCache.cs
@@ -25,10 +25,24 @@ namespace SharpUtilities
         /// </summary>
         /// <typeparam name="T">Type to be cached</typeparam>
         /// <param name="populateAction">The function that populates the cache on demand.</param>
-        /// <returns>Simple cache of &lt;T&gt;</returns>
+        /// <returns>Simple cache struct of &lt;T&gt;</returns>
         public static SimpleCacheStruct<T> CreateStruct<T>(Func<T> populateAction)
         {
             return new SimpleCacheStruct<T>(populateAction);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="SimpleCacheStruct{T}" /> class.
+        /// </summary>
+        /// <typeparam name="T">Type to be cached</typeparam>
+        /// <typeparam name="ContextType">Type of the context.</typeparam>
+        /// <param name="context">The context object</param>
+        /// <param name="populateAction">The function that populates the cache on demand.</param>
+        /// <returns>Simple cache with context of &lt;T&gt;</returns>
+        public static SimpleCacheWithContext<T, ContextType> CreateWithContext<T, ContextType>(ContextType context, Func<ContextType, T> populateAction)
+            where ContextType : class
+        {
+            return new SimpleCacheWithContext<T, ContextType>(context, populateAction);
         }
     }
 
@@ -73,7 +87,7 @@ namespace SharpUtilities
             get
             {
                 if (!Cached)
-                    lock(this)
+                    lock (this)
                         if (!Cached)
                         {
                             value = populateAction();
@@ -84,8 +98,11 @@ namespace SharpUtilities
 
             set
             {
-                this.value = value;
-                Cached = true;
+                lock (populateAction)
+                {
+                    this.value = value;
+                    Cached = true;
+                }
             }
         }
 
@@ -128,8 +145,11 @@ namespace SharpUtilities
     }
 
     /// <summary>
-    /// Helper class for caching results - it is being used as lazy evaluation
+    /// Helper structure for caching results - it is being used as lazy evaluation
     /// </summary>
+    /// <remarks>
+    /// This variant is meant to be used when smaller amount of memory is supposed to be used or less allocations.
+    /// </remarks>
     /// <typeparam name="T">Type to be cached</typeparam>
     public struct SimpleCacheStruct<T> : ICache
     {
@@ -181,8 +201,11 @@ namespace SharpUtilities
 
             set
             {
-                this.value = value;
-                Cached = true;
+                lock (populateAction)
+                {
+                    this.value = value;
+                    Cached = true;
+                }
             }
         }
 
@@ -195,6 +218,141 @@ namespace SharpUtilities
             {
                 if (Cached)
                     lock (populateAction)
+                        if (Cached)
+                        {
+                            ((IDisposable)value)?.Dispose();
+                            Cached = false;
+                        }
+            }
+            else
+                Cached = false;
+        }
+
+        /// <summary>
+        /// Gets enumerator for all the cached objects in this cache.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator()
+        {
+            if (Cached)
+                yield return value;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            InvalidateCache();
+        }
+    }
+
+    /// <summary>
+    /// Helper structure for caching results - it is being used as lazy evaluation.
+    /// </summary>
+    /// <remarks>
+    /// This variant is meant to be used when minimal memory supposed to be used - when lots of objects
+    /// of the same time should be created in the runtime.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// class Test
+    /// {
+    ///     private Func&lt;Test, int&gt; staticDelegate = (t) => t.LazyInitialization();
+    ///     private SimpleCacheWithContext&lt;int, Test&gt; cache;
+    ///
+    ///     public Test()
+    ///     {
+    ///         cache = SimpleCache.CreateWithContext(staticDelegate, this);
+    ///     }
+    ///
+    ///     public int Data => cache.Value;
+    ///
+    ///     private int LazyInitialization()
+    ///     {
+    ///         // Some complex long lasting lazy evaluation
+    ///         return 42;
+    ///     }
+    /// }
+    /// </code>
+    /// </example>
+    /// <typeparam name="T">Type to be cached</typeparam>
+    /// <typeparam name="ContextType">Type of the context.</typeparam>
+    public struct SimpleCacheWithContext<T, ContextType> : ICache
+        where ContextType : class
+    {
+        /// <summary>
+        /// The populate action
+        /// </summary>
+        private readonly Func<ContextType, T> populateAction;
+
+        /// <summary>
+        /// The context object.
+        /// </summary>
+        private readonly ContextType context;
+
+        /// <summary>
+        /// The value that is cached
+        /// </summary>
+        private T value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleCacheWithContext{T, ContextType}"/> class.
+        /// </summary>
+        /// <param name="context">The context object</param>
+        /// <param name="populateAction">The function that populates the cache on demand.</param>
+        public SimpleCacheWithContext(ContextType context, Func<ContextType, T> populateAction)
+        {
+            this.populateAction = populateAction;
+            this.context = context;
+            value = default(T);
+            Cached = false;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether value is cached.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if cached; otherwise, <c>false</c>.
+        /// </value>
+        public bool Cached { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the value. The value will be populated if it wasn't cached.
+        /// </summary>
+        public T Value
+        {
+            get
+            {
+                if (!Cached)
+                    lock (context ?? (object)populateAction)
+                        if (!Cached)
+                        {
+                            value = populateAction(context);
+                            Cached = true;
+                        }
+                return value;
+            }
+
+            set
+            {
+                lock (context ?? (object)populateAction)
+                {
+                    this.value = value;
+                    Cached = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Invalidate cache entry.
+        /// </summary>
+        public void InvalidateCache()
+        {
+            if (typeof(IDisposable).IsAssignableFrom(typeof(T)))
+            {
+                if (Cached)
+                    lock (context ?? (object)populateAction)
                         if (Cached)
                         {
                             ((IDisposable)value)?.Dispose();
--- Tests/SharpUtilities.Tests/SimpleCacheTests.cs
+++ Tests/SharpUtilities.Tests/SimpleCacheTests.cs
@@ -208,4 +208,114 @@ namespace SharpUtilities.Tests
             Assert.Equal(2, disposedCount);
         }
     }
+
+    public class SimpleCacheWithContextTests
+    {
+        private class Context
+        {
+            public int Count;
+        }
+
+        [Fact]
+        public void SingleEvaluation()
+        {
+            Context context = new Context();
+            SimpleCacheWithContext<int, Context> cache = SimpleCache.CreateWithContext(context, (c) =>
+            {
+                c.Count++;
+                return 42;
+            });
+            Assert.Equal(0, context.Count);
+            Assert.False(cache.Cached);
+            Assert.Equal(42, cache.Value);
+            Assert.Equal(1, context.Count);
+            Assert.True(cache.Cached);
+            Assert.Equal(42, cache.Value);
+            Assert.Equal(1, context.Count);
+            Assert.Single(cache, 42);
+        }
+
+        [Fact]
+        public void EvaluationAfterInvalidate()
+        {
+            Context context = new Context();
+            SimpleCacheWithContext<int, Context> cache = SimpleCache.CreateWithContext(context, (c) =>
+            {
+                c.Count++;
+                return 42;
+            });
+            Assert.Equal(0, context.Count);
+            Assert.Equal(42, cache.Value);
+            Assert.Equal(1, context.Count);
+            Assert.Equal(42, cache.Value);
+            Assert.Equal(1, context.Count);
+            cache.InvalidateCache();
+            Assert.Equal(1, context.Count);
+            Assert.Equal(42, cache.Value);
+            Assert.Equal(2, context.Count);
+        }
+
+        [Fact]
+        public void SettingValue()
+        {
+            Context context = new Context();
+            SimpleCacheWithContext<int, Context> cache = SimpleCache.CreateWithContext(context, (c) =>
+            {
+                c.Count++;
+                return 42;
+            });
+            Assert.Equal(0, context.Count);
+            Assert.Equal(42, cache.Value);
+            Assert.Equal(1, context.Count);
+            cache.Value = 12345;
+            Assert.Equal(12345, cache.Value);
+            Assert.Equal(1, context.Count);
+            cache.InvalidateCache();
+            Assert.Equal(1, context.Count);
+            Assert.Equal(42, cache.Value);
+            Assert.Equal(2, context.Count);
+        }
+
+        [Fact]
+        public void Assignment()
+        {
+            Context context = new Context();
+            SimpleCacheWithContext<int, Context> cache = SimpleCache.CreateWithContext(context, (c) =>
+            {
+                c.Count++;
+                return 42;
+            });
+            var cache2 = cache;
+            Assert.Equal(0, context.Count);
+            Assert.False(cache.Cached);
+            Assert.Equal(42, cache.Value);
+            Assert.Equal(1, context.Count);
+            Assert.True(cache.Cached);
+            Assert.False(cache2.Cached);
+            Assert.Equal(42, cache2.Value);
+            Assert.Equal(2, context.Count);
+            Assert.True(cache2.Cached);
+            var cache3 = cache;
+            Assert.True(cache3.Cached);
+            Assert.Equal(42, cache3.Value);
+            Assert.Equal(2, context.Count);
+        }
+
+        [Fact]
+        public void Disposable()
+        {
+            Context context = new Context();
+            int disposedCount = 0;
+            using (SimpleCacheWithContext<DisposableAction, Context> cache = SimpleCache.CreateWithContext(context, (c) => new DisposableAction(() => disposedCount++)))
+            {
+                var v0 = cache.Value;
+
+                cache.InvalidateCache();
+                Assert.Equal(1, disposedCount);
+
+                var v1 = cache.Value;
+            }
+            Assert.Equal(2, disposedCount);
+        }
+    }
 }
